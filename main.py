5cb2a5fbd50624e53961d1d246262cae58472c33
belorusK <156575616+belorusK@users.noreply.github.com>
2024-01-21 16:47:49 +0300

    password_generator2.0

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import random
+print('PASSWORD GENERATOR')
+while True:
 
-symbol_password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-length_password = int(input('Введите длину пароля: '))
-gen_password = ''
+    import random
 
-for i in range(length_password):
-    gen_password += random.choice(symbol_password)
-print(gen_password)
+    
+    symbol_password = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    length_password = int(input('Введите длину пароля: '))
+    gen_password = ''
+
+    for i in range(length_password):
+        gen_password += random.choice(symbol_password)
+    print(gen_password)
